778b4b34925ff24a0a2e15e1e906316c9abe954a
Esther Walker <equestriennesrule@yahoo.com>
2020-03-30 11:55:37 -0700

    made some changes but got confused about them

--- server.js
+++ server.js
@@ -2,8 +2,17 @@
 
 const express = require('express');
 const app = express();
+// const compression = require("compression");
 const PORT = process.env.PORT || 3000;
 
+// app.use(compression({filter: shouldCompress}))
+
+// function shouldCompress (req, res) {
+//     if (req.headers['x-no-compression']) {
+//         return false
+//     }
+//     return compression.filter(req, res)
+// }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
